dbcaa0b491e9d7317d0a8b9ec09eef1706b04cd4
thorbergurharal <89218402+thorbergurharaldsson@users.noreply.github.com>
2021-09-15 21:03:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// This is a single line comment
+
+/*
+  This is a
+  multi line comment
+*/
+
+// Declaring a variable called helloWorld and assigning a string to it
+var helloWorld = "Hello, world!"
+
+// Logs a value to the console, in this case, the variable helloWorld
+console.log(helloWorld)
+
+// This is a function that shows an alert when called
+function alertBox() {
+  alert("This is an alert")
+}
+
+// function that changes the content of a html tag, in this casae it searches for an element with the id = "changeText" and changes the innerHTML when called
+function chageText(){
+  document.getElementById("changeText").innerHTML = "Paragraph changed!";
+}
